66d03a9fac8db5e38bc6e8c6c82f138087a2e9af
Yunus Emre Gültepe <118046112+Mordris@users.noreply.github.com>
2023-06-09 22:24:57 +0300

    Update AdminController.cs

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -13,29 +13,12 @@ namespace Social_Media_API.Controllers
         public IActionResult GetAdminData()
         {
             // This action is only accessible to users with the "Admin" role
-            // Implement your logic here
-
             return Ok("Admin data");
         }
 
         [HttpPost]
         public IActionResult CreateAdminUser([FromBody] AdminUserModel user)
         {
-            // This action is only accessible to users with the "Admin" role
-            // Implement your logic here to create an admin user
-            // The user parameter is received from the request body
-
-            // Example: Create admin user code
-            // var newUser = new User
-            // {
-            //     Username = user.Username,
-            //     Password = user.Password,
-            //     Email = user.Email,
-            //     FullName = user.FullName,
-            //     Role = "Admin"
-            // };
-            // // Save the new user to the database
-
             return Ok("Admin user created");
         }
     }
